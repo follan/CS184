48118cd3e42c38f23664ed17ca3fbef6f80abcdc
Emil Lunde <emil_lunde@hotmail.com>
2013-11-04 22:11:53 -0800

    GUI.cpp now displays a red polygon

--- as2_git/as2_git/GUI.cpp
+++ as2_git/as2_git/GUI.cpp
@@ -6,38 +6,48 @@ GUI::~GUI(void)
 
 void setup()
 {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //clear the buffers
-
 	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glFrustum(-1.0, 1.0, -1.0, 1.0, 0.0, -1.0); //defines the frustum by left, right, bottom, top, near and far
 }
 
 
-void customDisplayFunc()
+void renderScene()
 {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //clear the buffers
+
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
 	//we just draw something to make sure the basics work
 	glBegin(GL_POLYGON);
 	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex2f(-1.0f, 1.0f);
-	glVertex2f(-1.0f, -1.0f);
-	glVertex2f(1.0f, -1.0f);
-	glVertex2f(1.0f, 1.0f);
+	glVertex3f(-0.5f, 0.5f, -0.5f);
+	glVertex3f(-0.5f, -0.5f, -0.5f);
+	glVertex3f(0.5f, -0.5f, -0.5f);
+	glVertex3f(0.5f, 0.5f, -0.5f);
 	glEnd();
 
 	glFlush(); //flush the buffer so things are actually drawn
+	glutSwapBuffers();
 }
+
+void reshapeScene(int widht, int height)
+{
+	//do something when the window is resized
+}
+
 
 
 int main(int argc ,char* argv[])
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH);
+	glutInitDisplayMode(GLUT_DOUBLE |GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(400, 400);
 	glutInitWindowPosition(0, 0);
 	glutCreateWindow("Assignment3");
-	glutDisplayFunc(customDisplayFunc);
-	//glutReshapeFunc(myReshape);
+	setup();
+	glutDisplayFunc(renderScene);
+	glutReshapeFunc(reshapeScene);
 	glutMainLoop();
 }
