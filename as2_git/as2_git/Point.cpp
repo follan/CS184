becb2c2d63f61d17d1f5d5c1a4f2006a09a65714
Follan <tobbis.follan@gmail.com>
2013-11-04 20:28:05 -0800

    added operator functions

--- as2_git/as2_git/Point.cpp
+++ as2_git/as2_git/Point.cpp
@@ -27,6 +27,24 @@ float Point::getZ() const
 	return m_z;
 }
 
+Point Point::operator+(const Point a_p)const
+{
+	return Point(m_x+a_p.getX(), m_y+a_p.getY(), m_z+a_p.getZ());
+}
+
+Point Point::operator-(const Point a_p) const
+{
+	return Point(m_x-a_p.getX(), m_y-a_p.getY(), m_z-a_p.getZ());
+}
+
+
+Point Point::operator*(const float a_c) const
+{
+	return Point(m_x*a_c, m_y*a_c, m_z*a_c);
+}
+
+
+
 Point::~Point(void)
 {
 }
--- as2_git/as2_git/Point.h
+++ as2_git/as2_git/Point.h
@@ -6,6 +6,10 @@ public:
 	Point(float a_x, float a_y, float a_z);
 	~Point(void);
 
+	Point operator+(const Point a_p) const;
+	Point operator*(const float a_c) const;
+	Point operator-(const Point a_p) const;
+
 	float getX() const;
 	float getY() const;
 	float getZ() const;
