a4b37adcbf074b56a842556e87cac6440d377809
Follan <tobbis.follan@gmail.com>
2013-11-06 19:54:23 -0800

    added Triangle class and headet

--- as2_git/as2_git/AdaptiveSub.cpp
+++ as2_git/as2_git/AdaptiveSub.cpp
@@ -9,3 +9,8 @@ AdaptiveSub::AdaptiveSub(void)
 AdaptiveSub::~AdaptiveSub(void)
 {
 }
+
+void AdaptiveSub::subDividePatch(const Patch& a_patch, vector<Point> a_drawPoints)
+{
+
+}
--- as2_git/as2_git/AdaptiveSub.h
+++ as2_git/as2_git/AdaptiveSub.h
@@ -1,8 +1,14 @@
 #pragma once
+#include "Patch.h"
+#include "Normal.h"
+#include <iostream>
+#include "Parser.h"
+#include <sstream>
 class AdaptiveSub
 {
 public:
 	AdaptiveSub(void);
 	~AdaptiveSub(void);
+	static void subDividePatch(const Patch& a_patch, vector<Point> a_drawPoints);
 };
 
